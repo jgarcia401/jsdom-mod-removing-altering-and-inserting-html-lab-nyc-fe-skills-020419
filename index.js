5ea8f25a38bf514880555c04014fa2571e472d91
resourceful-transpiler-3397 <jgarcia401@gmail.com>
2019-03-07 00:14:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
 
 let myMain = document.querySelector('#main')
 
 myMain.remove()
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = 'Jeremy is the champion!'
+
